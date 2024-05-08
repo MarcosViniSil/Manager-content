60a9e6b4790b3510435f790cb87f10f532223f0a
MarcosViniSil <Marcossilv203@gmail.com>
2024-05-08 18:38:33 -0300

    feat cria endpoint atualiza conteudo

--- api/controller/manipulationContent.js
+++ api/controller/manipulationContent.js
@@ -1,12 +1,15 @@
 import express from "express";
 import projectsUser from "../services/addContentADM.js";
 import fetchContents from "../services/fetchContentsAdm.js";
+import updateContent from "../services/updateContent.js";
+import fetchContent from "../services/fetchContent.js";
 
 const router = express.Router();
 
 router.use(express.json());
 
 router.post('/validate/adm', projectsUser);
-router.post('/contents', fetchContents);
-
+router.post('/contents/adm', fetchContents);
+router.post('/update/content', updateContent);
+router.post('/content/id', fetchContent);
 export default router;
---
+++ api/services/updateContent.js
@@ -0,0 +1,47 @@
+import supabase from "../repositories/connectionSupabase.js";
+import email from "nodemailer"
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const updateContent = async (req, res) => {
+  const { data, error } = await supabase
+    .from("field")
+    .update({ content: req.body.content })
+    .eq("id", req.body.id)
+  if (error) {
+    res.status("Erro ao buscar dados:").send("erro");
+  } else {
+    sendEmail(req.body.id,req.body.content)
+    res.send("ok");
+  }
+};
+
+
+function sendEmail(id,content){
+    var transporter = email.createTransport({
+        service: 'gmail',
+        auth: {
+          user: process.env.EMAILSENDER,
+          pass: process.env.PASSWORDSENDER
+        }
+      });
+      const currentDate = new Date().toLocaleDateString('en-GB')
+      var mailOptions = {
+        from: process.env.EMAILSENDER,
+        to: process.env.EMAILRECIVE,
+    
+        subject: `Alteração conteúdo dia ${currentDate}`,
+        text:  `O id ${id} foi alterado para: ${content}`
+      };
+      
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent:');
+        }
+      });
+}
+
+export default updateContent;
