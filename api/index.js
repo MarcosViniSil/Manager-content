f46fa0f6ed4cc70c35d72ebf0b73c025d0e5b917
MarcosViniSil <Marcossilv203@gmail.com>
2024-05-09 19:03:22 -0300

    docs: adiciona documentação em html

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors  from 'cors'
 import userController from './controller/manipulationContent.js';
-
+import fs from "fs"
 dotenv.config();
 
 const app = express();
@@ -14,6 +14,12 @@ app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3000;
 app.use('/', userController);
+app.get('/', (req, res) => {
+
+  fs.readFile('./index.html', 'utf8', (err, data) => {  
+    res.send(data);
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
