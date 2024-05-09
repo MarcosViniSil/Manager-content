1d3c719ab4d8bf18366d48ade1e3604f61d88b76
MarcosViniSil <Marcossilv203@gmail.com>
2024-05-09 18:07:13 -0300

    feat: cria endpoint cryptografar senha

---
+++ api/services/encryptPassword.js
@@ -0,0 +1,11 @@
+import encryptpwd  from "encrypt-with-password"
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const fetchContents = async (req, res) => {
+    const encrypted = encryptpwd.encrypt(req.body.password, process.env.PASSWORDCRYPTO); 
+    res.send(encrypted)
+};
+
+export default fetchContents;
