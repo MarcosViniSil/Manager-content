63aa7b6dd72cf2303c2d7fd25962289f6ca51c96
MarcosViniSil <Marcossilv203@gmail.com>
2024-05-09 18:08:08 -0300

    feat: cria endpoint decryptografar senha

--- api/controller/manipulationContent.js
+++ api/controller/manipulationContent.js
@@ -3,7 +3,8 @@ import projectsUser from "../services/getIdAdm.js";
 import fetchContents from "../services/fetchContentsAdm.js";
 import updateContent from "../services/updateContent.js";
 import fetchContent from "../services/fetchContent.js";
-
+import crypt from "../services/encryptPassword.js"
+import decrypt from "../services/decryptPassword.js"
 const router = express.Router();
 
 router.use(express.json());
@@ -12,4 +13,6 @@ router.post('/validate/adm', projectsUser);
 router.post('/contents/adm', fetchContents);
 router.post('/update/content', updateContent);
 router.post('/content/id', fetchContent);
+router.post('/encrypt/password', crypt);
+router.post('/decrypt/password', decrypt);
 export default router;
---
+++ api/services/decryptPassword.js
@@ -0,0 +1,11 @@
+import encryptpwd  from "encrypt-with-password"
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const decryptPassword = async (req, res) => {
+    const decrypted = encryptpwd.decrypt(req.body.code, process.env.PASSWORDCRYPTO)
+    res.send(decrypted)
+};
+
+export default decryptPassword;
