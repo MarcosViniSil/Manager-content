08310572b38a77b308befa5c1d477ffd1fa679e7
MarcosViniSil <Marcossilv203@gmail.com>
2024-05-10 16:52:46 -0300

    chore: atualiza retorno criptografia de senha

--- api/services/decryptPassword.js
+++ api/services/decryptPassword.js
@@ -5,7 +5,10 @@ dotenv.config();
 
 const decryptPassword = async (req, res) => {
     const decrypted = encryptpwd.decrypt(req.body.code.toString(), process.env.PASSWORDCRYPTO)
-    res.send(decrypted)
+    const response = {
+        passwordDecrypted: decrypted
+    }
+    res.send(JSON.stringify(response))
 };
 
 export default decryptPassword;
--- api/services/encryptPassword.js
+++ api/services/encryptPassword.js
@@ -5,7 +5,10 @@ dotenv.config();
 
 const fetchContents = async (req, res) => {
     const encrypted = encryptpwd.encrypt(req.body.password.toString(), process.env.PASSWORDCRYPTO); 
-    res.send(encrypted)
+    const response = {
+        passwordCrypted: encrypted
+    }
+    res.send(JSON.stringify(response))
 };
 
 export default fetchContents;
