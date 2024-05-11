ebd93d5cbdac186a694ea1494fcd4c1096e06017
MarcosViniSil <Marcossilv203@gmail.com>
2024-05-11 15:29:20 -0300

    chore: atualiza envio email

--- api/services/updateContent.js
+++ api/services/updateContent.js
@@ -1,7 +1,6 @@
 import supabase from "../repositories/connectionSupabase.js";
-import email from "nodemailer"
+import email from "nodemailer";
 import dotenv from "dotenv";
-import sendEmail from "./sendEmail.js";
 
 dotenv.config();
 
@@ -9,14 +8,55 @@ const updateContent = async (req, res) => {
   const { data, error } = await supabase
     .from("field")
     .update({ content: req.body.content })
-    .eq("id", req.body.id)
+    .eq("id", req.body.id);
   if (error) {
     res.status("Erro ao buscar dados:").send("erro");
   } else {
-    await sendEmail(req,res,req.body.id,req.body.content)
+    await sendEmail(req.body.id, req.body.content);
     res.send("ok");
   }
 };
+async function sendEmail(id, content) {
+  const transporter = email.createTransport({
+    port: 465,
+    host: "smtp.gmail.com",
+    auth: {
+      user: process.env.EMAILSENDER,
+      pass: process.env.PASSWORDSENDER,
+    },
+    secure: true,
+  });
 
+  await new Promise((resolve, reject) => {
+    // verify connection configuration
+    transporter.verify(function (error, success) {
+      if (error) {
+        console.log(error);
+        reject(error);
+      } else {
+        console.log("Server is ready to take our messages");
+        resolve(success);
+      }
+    });
+  });
+  const currentDate = new Date().toLocaleDateString("en-GB");
+  const mailData = {
+    from: process.env.EMAILSENDER,
+    to: process.env.EMAILRECIVE,
+    subject: `Alteração conteúdo dia ${currentDate}`,
+    text: `O id ${id} foi alterado para: ${content}`,
+  };
+
+  await new Promise((resolve, reject) => {
+
+    transporter.sendMail(mailData, (err, info) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(info);
+      }
+    });
+  });
+}
 
 export default updateContent;
